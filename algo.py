972ea181c534ae91ec95e7c40cb75f8d3f1bbba7
balogun14 <awwalbalogun06@gmail.com>
2023-07-16 12:30:11 +0100

    commit

--- algo.py
+++ algo.py
@@ -5,6 +5,7 @@ def insertEnd(arr, n, length, capacity):
     if length < capacity:
         arr[length] = n
 
+
 # Remove from the last position in the array if the array
 # is not empty (i.e. length is non-zero).
 def removeEnd(arr, length):
@@ -13,15 +14,17 @@ def removeEnd(arr, length):
         # We would also the length to decreased by 1.
         arr[length - 1] = 0
 
+
 # Insert n into index i after shifting elements to the right.
 # Assuming i is a valid index and arr is not full.
 def insertMiddle(arr, i, n, length):
     # Shift starting from the end to i.
     for index in range(length - 1, i - 1, -1):
         arr[index + 1] = arr[index]
-    
+
     # Insert at i
     arr[i] = n
+
 
 # Remove value at index i before shifting elements to the left.
 # Assuming i is a valid index.
@@ -30,6 +33,7 @@ def removeMiddle(arr, i, length):
     for index in range(i + 1, length):
         arr[index - 1] = arr[index]
     # No need to 'remove' arr[i], since we already shifted
+
 
 def printArr(arr, capacity):
     for i in range(capacity):
